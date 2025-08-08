9165218371c002e4590a0b6bf27b74d817a92bb9
CallmeTcee <emeka.tochukwu11@gmail.com>
2025-08-08 13:34:10 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~tq9jzQ5R#0oAFCvF5ZHYX478a3uKh5MplbhtezdvHPuQuYEuLyGM",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
